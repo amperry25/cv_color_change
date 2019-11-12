0c60c310f390b867de9da99f97a0179e2bb3711d
Alex Perry <perry.am@pg.com>
2019-11-12 10:29:03 -0500

    update
    updated block

--- cv_color_change_block.py
+++ cv_color_change_block.py
@@ -1,12 +1,16 @@
+import numpy
+import cv2
+
 from nio import Block
-from nio.properties import VersionProperty
+from nio.properties import Property, VersionProperty
 
 
 class CvColorChange(Block):
 
     version = VersionProperty('0.1.0')
+    input_array = Property(title='Array to Convert', default='{{ $image }}')
 
-    def process_signals(self, signals):
+    def process_signals(self, signals, input_id='default'):
         for signal in signals:
-            pass
+            signal.image = cv2.cvtColor(self.input_array(signal), cv2.COLOR_BGR2RGB)
         self.notify_signals(signals)
